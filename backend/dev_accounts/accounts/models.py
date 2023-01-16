b2788d9b7ac386fd1f59b13478abdee44c83b8c0
dreamjh1111 <dreamjh1111@naver.com>
2023-01-16 15:21:22 +0900

    feat: accounts url 설정 && accounts models 생성 - 초기 dj_rest_auth url 연결 - account model 생성

--- backend/dev_accounts/accounts/models.py
+++ backend/dev_accounts/accounts/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import UserManager
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    #user_id = models.CharField(max_length=100),
+    #nickname = models.CharField(max_length=50),
+    #login_type = models.IntegerField(default=0),
+    #stat = models.IntegerField(default=0),
+
+    
+
+
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
