e451640011e63c00aab2c7c6f99734c4ff64a216
jina <jina.oh.dev@gmail.com>
2023-01-16 14:17:50 +0900

    전체 태그 조회

--- backend/dev_blog/blogs/views.py
+++ backend/dev_blog/blogs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ArticleSerializer, CommentSerializer, TagSerializer
+from .models import Article, Comment, Tag
 
-# Create your views here.
+
+@api_view(['GET']) #전체 태그 조회
+def tag_all(request):
+    tags = Tag.objects.all()
+    serializer = TagSerializer(tags, many=True)
+    return Response(serializer.data)
