44fa57983b5f00f38d8fe221e97e39a74b905f31
jina <jina.oh.dev@gmail.com>
2023-01-16 14:19:29 +0900

    feat : 태그 연결된 게시글 조회

--- backend/dev_blog/blogs/views.py
+++ backend/dev_blog/blogs/views.py
@@ -16,3 +16,18 @@ def tag_list(request, search_tag):
     tags = Tag.objects.filter(tag__contains=search_tag)
     serializer = TagSerializer(tags, many=True)
     return Response(serializer.data)
+
+@api_view(['GET']) #태그 연결된 게시글 조회
+def tag_articles(request, search_tag): #태그 게시글 조회
+    try: #해당 태그 없을 때 
+        tag = Tag.objects.get(tag=search_tag)
+    except:
+        return Response(None)
+    # 해당 태그 있을 때 게시글 찾기
+    tag_articles = Article.objects.filter(tags=tag)
+    if tag_articles :
+        tag_articles = tag_articles.order_by('-created_at')
+        serializer = ArticleSerializer(tag_articles, many = True)
+        return Response(serializer.data)
+    else:
+        return Response(None)
