1092763ed7809ab80977b6e3edbc30fd598d1708
jina <jina.oh.dev@gmail.com>
2023-01-16 16:47:12 +0900

    refactor : 태그 검색+전체조회 / feat : 태그 여러개로 게시글 조회

--- backend/dev_blog/blogs/views.py
+++ backend/dev_blog/blogs/views.py
@@ -6,33 +6,38 @@ from .models import Article, Comment, Tag
 
 ############ 태그
 
-@api_view(['GET']) #전체 태그 조회
-def tag_all(request):
-    tags = Tag.objects.all()
-    serializer = TagSerializer(tags, many=True)
-    return Response(serializer.data)
-
 @api_view(['GET']) #태그 검색
-def tag_list(request, search_tag): 
-    tags = Tag.objects.filter(tag__contains=search_tag)
+def tag_list(request, search_tag=None): 
+    if search_tag: #태그 검색 조회
+        tags = Tag.objects.filter(tag__contains=search_tag)
+    else: #태그 전체 조회
+        tags = Tag.objects.all()
     serializer = TagSerializer(tags, many=True)
     return Response(serializer.data)
 
-@api_view(['GET']) #태그 연결된 게시글 조회
-def tag_articles(request, search_tag): #태그 게시글 조회
-    try: #해당 태그 없을 때 
-        tag = Tag.objects.get(tag=search_tag)
-    except:
-        return Response(None)
-    # 해당 태그 있을 때 게시글 찾기
-    tag_articles = Article.objects.filter(tags=tag)
-    if tag_articles :
-        tag_articles = tag_articles.order_by('-created_at')
-        serializer = ArticleSerializer(tag_articles, many = True)
-        return Response(serializer.data)
+
+@api_view(['GET'])
+def tag_articles(request): #태그 게시글 조회
+    select_tags = request.GET.getlist('tags')
+    temp_articles=[]
+    for search_tag in select_tags: 
+        try: #태그 없으면 넘어가도록 함
+            tag = Tag.objects.get(tag=search_tag)
+        except:
+            continue
+        tag_articles = Article.objects.filter(tags=tag)
+        if tag_articles :
+            for article in tag_articles:
+                temp_articles.append(article)
+    sort_article=[]
+    for article in temp_articles:
+        serializer = ArticleSerializer(article)
+        sort_article.append(serializer.data)
+    sort_article=sorted(sort_article , key= lambda x: -x['hit'])
+    if sort_article:
+        return Response(sort_article)
     else:
         return Response(None)
-
 
 ############### 게시글
 
@@ -43,61 +48,62 @@ def article_list(request): #전체게시판 조회
     return Response(serializer.data)
 
 
-@api_view(['POST'])
-def article_create(request): #글작성
-    entered_tags = request.POST.getlist("tags")
-    data = {
-        'title' : request.data['title'],
-        'content' : request.data['content'],
-    }
-    if not entered_tags:
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            # serializer.save(user = request.user)  
-            serializer.save()  
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-    tags = []
-    for tag in entered_tags :
-        temp, already_tag = Tag.objects.get_or_create(tag=tag)
-        tags.append(temp)
-    serializer = ArticleSerializer(data=data)
-    if serializer.is_valid():
-        # serializer.save(user = request.user)  
-        serializer.save(tags = tags)  
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+# 게시글 작성, 수정, 조회, 삭제
+@api_view(['POST','GET','PUT','DELETE'])
+def article(request, article_id=None): #게시글 디테일
+    if article_id:
+        article = Article.objects.get(pk=article_id)
+        if request.method == 'GET': #조회
+            article.hit+=1
+            article.save()
+            serializer = ArticleSerializer(article)
+            return Response(serializer.data)
+        
+        elif request.method == 'DELETE': #삭제
+            article.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
+        elif request.method == 'PUT': #수정
+            entered_tags = request.POST.getlist('tags')
+            data = {
+                'title' : request.data['title'],
+                'content' : request.data['content'],
+            }
+            if not entered_tags:
+                serializer = ArticleSerializer(article, data=data)
+                if serializer.is_valid(raise_exception=True):
+                    # serializer.save(user = request.user)  
+                    serializer.save(tags=[])  
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                tags = []
+                for tag in entered_tags :
+                    temp, already_tag = Tag.objects.get_or_create(tag=tag)
+                    tags.append(temp)
+                serializer = ArticleSerializer(article,data=data)
+                if serializer.is_valid():
+                    # serializer.save(user = request.user)  
+                    serializer.save(tags = tags)  
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET','PUT','DELETE'])
-def article(request, article_id): #게시글 디테일
-    article = Article.objects.get(pk=article_id)
-    if request.method == 'GET': #조회
-        article.hit+=1
-        article.save()
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-    
-    elif request.method == 'DELETE': #삭제
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    elif request.method == 'PUT': #수정
-        entered_tags = request.POST.getlist('tags')
-        data = {
-            'title' : request.data['title'],
-            'content' : request.data['content'],
-        }
-        if not entered_tags:
-            serializer = ArticleSerializer(article, data=data)
-            if serializer.is_valid(raise_exception=True):
-                # serializer.save(user = request.user)  
-                serializer.save(tags=[])  
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
+    else :     
+        if request.method == 'POST': #게시글 작성
+            entered_tags = request.POST.getlist("tags")
+            data = {
+                'title' : request.data['title'],
+                'content' : request.data['content'],
+            }
+            if not entered_tags:
+                serializer = ArticleSerializer(data=data)
+                if serializer.is_valid():
+                    # serializer.save(user = request.user)  
+                    serializer.save()  
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
             tags = []
             for tag in entered_tags :
                 temp, already_tag = Tag.objects.get_or_create(tag=tag)
                 tags.append(temp)
-            serializer = ArticleSerializer(article,data=data)
+            serializer = ArticleSerializer(data=data)
             if serializer.is_valid():
                 # serializer.save(user = request.user)  
                 serializer.save(tags = tags)  
