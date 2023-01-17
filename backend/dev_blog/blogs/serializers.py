d4b97f46a8692468e229ad0cf041ce635866a4b0
jina <jina.oh.dev@gmail.com>
2023-01-17 13:31:52 +0900

    feat : serializer에 like_users_count, comments_count 구현

--- backend/dev_blog/blogs/serializers.py
+++ backend/dev_blog/blogs/serializers.py
@@ -6,20 +6,26 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = '__all__'
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.nickname', read_only=True)
+    like_users_count = serializers.IntegerField(source = 'like_users.count', read_only=True)
+    comments_count = serializers.IntegerField(source = 'comments.count', read_only=True)
 
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('username','user', 'like_users', 'tags')
+        read_only_fields = ('username','user', 'like_users', 'tags', 'like_users_count', 'comments_count')
 
 
 class CommentSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.nickname', read_only=True)
-    
+    like_users_count = serializers.IntegerField(source = 'like_users.count', read_only=True)
+
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('user', 'article','parent_comment','lke_users')
+        read_only_fields = ('user', 'article','parent_comment','like_users', 'like_users_count')
+
+
 
