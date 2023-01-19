127f2a1930b680c81356e254d4ff64c1d71569e1
cloudsoswift <magic9282m@gmail.com>
2023-01-19 13:05:04 +0900

    feat: 질문 및 기능 설명에대한 skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import React, { Fragment } from "react";
+import IndexPage from "./pages/index/IndexPage";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <Fragment>
+      <Routes>
+        <Route index element={<IndexPage />} />
+      </Routes>
     </Fragment>
   );
 }
