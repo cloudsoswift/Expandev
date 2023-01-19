26907e12229c07fbbd6e772143772cd3b886b476
yonghoon <fishlittle2003@gmail.com>
2023-01-19 12:56:52 +0900

    stashing

--- frontend/morehae_app/src/App.js
+++ frontend/morehae_app/src/App.js
@@ -1,8 +1,20 @@
 import RoadmapPage from "./pages/RoadmapPage.jsx";
 import "./index.css"
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
 export default function App() {
   return (
-    <RoadmapPage />
+    <div>
+      <div className="flex justify-center">
+        <div className="w-1/2">
+          <SignUp />
+        </div>
+      </div>
+    </div>
+    
+    
+    // <SignIn />
+    // <RoadmapPage />
   );
 }
