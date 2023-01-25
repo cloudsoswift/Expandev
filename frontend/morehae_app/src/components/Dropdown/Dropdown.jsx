1bace7d43ecb7cae0f5d33020cd5792a83a159de
yonghoon <fishlittle2003@gmail.com>
2023-01-25 13:36:38 +0900

    refactoring dropdown and removing RoadmapPage.jsx

--- frontend/morehae_app/src/components/Dropdown/Dropdown.jsx
+++ frontend/morehae_app/src/components/Dropdown/Dropdown.jsx
@@ -1,34 +1,81 @@
-import {BsChevronDown} from 'react-icons/bs'
-import { useEffect } from 'react'
+/* single select만 가능한 드롭다운 컴포넌트 */
 
-const Dropdown = ({isOpenRole, setIsOpenRole, selectedRole, setSelectedRole, roleList}) => {
-  const msg = "희망 직군을 선택해주세요";
+import { useState, useEffect, useRef } from 'react'
+import { BsChevronDown } from 'react-icons/bs'
+
+// items: 리스팅 될 아이템들, selectedItem: 선택할 아이템 객체, setSelectedItem: 아이템 객체에 대한 setter
+function Dropdown({items, selectedItem, setSelectedItem}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  // 드롭다운 클릭에 대한 핸들러
+  const handleOpen = () => {
+    // 드롭다운 창 닫기
+    setIsOpen((oldState) => {
+      return !oldState;
+    })
+  }
+
+  // 아이템 클릭에 대한 핸들러
+  const handleSelect = (e) => {
+    const index = e.target.value
+    if (index === undefined) {  // 가끔씩, 정말 아주 가끔씩 e.target이 <span> 태그를 가리켜
+      console.log("?????????????????");  // e.target.value === undefiend가 되는 경우가 있다
+      console.log(e);
+    } else {
+      setSelectedItem((oldState) => {
+        return items[index];
+      })
+    }
+    
+    // 드롭다운 창 닫기
+    setIsOpen((oldState) => {
+      return !oldState;
+    })
+  }
+
   useEffect(() => {
-    // console.log("드롭다운 렌더링됨", isOpenRole);
-    console.log("렌더", roleList);
-  })
-  
-  function clickHandler() {
-    setIsOpenRole(!isOpenRole);
-  }
+    // 마우스 클릭이 드롭다운 컴포넌트 외부에 찍혔는지를 판단하기 위한 핸들러
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setIsOpen(() => {
+          return false;
+        });
+      }
+    }
 
-  function selected(e) {
-    // console.log(roleListDummy[e.target?.value]);
-    setIsOpenRole(!isOpenRole);
-    setSelectedRole(e.target?.value)
-  }
+    // 핸들러 등록
+    document.addEventListener("mousedown", handler);
+
+    // 핸들러 등록 해제
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    }
+  }, [])
 
   return (
-    <div className="relative w-full">
-      <button onClick={clickHandler} className="cursor-default h-12 bg-red-100 flex items-center w-full justify-between p-4">
-        {msg}
-        <BsChevronDown />
+    <div className="flex-col relative w-full" ref={menuRef}>
+      <button onClick={handleOpen} className="transition bg-white w-full p-4 rounded-lg border hover:border-blue-500 flex justify-between font-extralight items-center">
+        <div>{selectedItem.content}</div>
+        <div><BsChevronDown/></div>
       </button>
-      {isOpenRole && 
-      <ul className="absolute w-full bg-blue-300 max-h-60 overflow-y-auto">
-        {roleList.map((role, index)=>(
-          <li key={role.id} value={index} onClick={selected} className="p-2 hover:bg-yellow-300">{role.content}</li>
-        ))}
+      { isOpen && 
+      <ul className="overflow-y-auto max-h-96 bg-white absolute w-full rounded-lg p-2 mt-2 border" style={{boxShadow: '1px 2px 9px rgba(0, 0, 0, 0.07)'}}>
+        {items.map((item, index) => {
+          let style = "transition p-4 rounded-lg cursor-pointer font-extralight";
+          if (item.id === selectedItem.id) {
+            style += " bg-blue-500 text-white"
+          } else {
+            style += " hover:bg-gray-100"
+          }
+          return <li
+            value={index}
+            key={item.id}
+            onClick={handleSelect}
+            className={style}>
+              <span>{item.content}</span>
+            </li>
+        })}
       </ul>
       }
     </div>
