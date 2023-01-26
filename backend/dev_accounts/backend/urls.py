a6211a14bd276ef18ce1036d1606b305cd385ef3
JAY-Winter <rubat0@naver.com>
2023-01-26 13:08:52 +0900

    feat: Monolithic 아키텍쳐 전환

--- backend/dev_accounts/backend/urls.py
+++ backend/dev_accounts/backend/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('blogs/', include('blogs.urls')),
+    path('roadmaps/', include('roadmaps.urls')),
 ]
