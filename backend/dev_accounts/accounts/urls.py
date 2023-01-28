1b5c1821f016707609475e43cff6df60f8661750
JAY-Winter <rubat0@naver.com>
2023-01-29 03:39:34 +0900

    feat: user profile image 구현

--- backend/dev_accounts/accounts/urls.py
+++ backend/dev_accounts/accounts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration', include('dj_rest_auth.registration.urls')),
     path('userlist', views.userlist),
-    path('userchange', views.userchange)
+    path('userchange', views.userchange),
+    path('user/<int:user_id>', views.get_user_profile),
+    path('profile/image', views.set_profile_image),
 ]
