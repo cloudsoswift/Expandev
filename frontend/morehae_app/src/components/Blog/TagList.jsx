ff035669cfb7f018ebb3add2a123113a472bf385
cloudsoswift <magic9282m@gmail.com>
2023-01-31 17:57:10 +0900

    fix: TOAST UI Editor가 여러 번 재 렌더링 되던 문제 fix 및 Tag List key 부여

--- frontend/morehae_app/src/components/Blog/TagList.jsx
+++ frontend/morehae_app/src/components/Blog/TagList.jsx
@@ -6,10 +6,9 @@ const TagList = ({TagList, onDelete}) => {
   const [tags, setTags] = useState([...TagList]);
   useEffect(()=>{
     setTags(TagList);
-    console.log(TagList);
   }, [TagList])
   return (
-    <div className="h-full space-x-1 space-y-">
+    <div className="h-full space-x-1">
       {tags.map((tag)=><TagPill key={tag} title={tag} onDelete={onDelete}/>)}
     </div>
   )
--- frontend/morehae_app/src/pages/Blog/BlogWritePage.jsx
+++ frontend/morehae_app/src/pages/Blog/BlogWritePage.jsx
@@ -1,4 +1,4 @@
-import { Editor } from "@toast-ui/editor";
+import Editor from "@toast-ui/editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -6,37 +6,57 @@ import http from "@/utils/http";
 import TagCombobox from "@/components/Blog/TagCombobox";
 import TagList from "../../components/Blog/TagList";
 import { useRef } from "react";
-
 const MAX_UPLOAD_IMAGE_SIZE = 5242880; // 1024 * 1024 * 5. 5MB로 제한
 
 const BlogWritePage = () => {
   const editorRef = useRef();
-  let editor = undefined;
+  const [editor, setEditor] = useState();
   const [title, setTitle] = useState("");
   const [tags, setTags] = useState([]);
+  const [isEditorRendered, setisEditorRendered] = useState(false);
   const hookMap = {
     addImageBlobHook: (blob, callback) => {
       // console.log(blob);
-
       // const u = URL.createObjectURL(blob);
       // console.log(u);
       // imageRef.current.src = u;
       // callback(u, "이미지");
+      alert("하이")
     },
   };
+
+  // editorRef가 할당되어 있는 element(<div ref={editorRef} />)가 mount 되어 있어야 new Editor()를 통해
+  // 에디터 생성하고 보여주는게 가능하므로 useEffect에서 new Editor를 통한 에디터 객체 생성.
   useEffect(() => {
-    editor = new Editor({
+    if (isEditorRendered){
+      return;
+    }
+    console.log("ㅎㅇ.");
+    // 컴포넌트 함수 재 호출(재 렌더링)시 editor 또 생성되는거 방지.
+    setEditor(
+      new Editor({
         el: editorRef.current,
         previewStyle: "vertical",
         height: "600px",
         hideModeSwitch: true,
         hooks: hookMap,
-      });
+        placeholder: "내용을 입력해주세요.",
+        // toolbarItems: [
+        //   // 툴바 옵션 설정
+        //   ["heading", "bold", "italic", "strike"],
+        //   ["hr", "quote"],
+        //   ["ul", "ol", "task", "indent", "outdent"],
+        //   ["table", "image", "link"],
+        //   ["code", "codeblock"],
+        // ],
+      })
+    );
+    setisEditorRendered(true);
   }, []);
 
   const handleTitleInputChange = (e) => {
     setTitle(e.target.value);
-    console.log(e);
+    console.log();
   };
 
   const handleSendPost = () => {
@@ -55,7 +75,7 @@ const BlogWritePage = () => {
       })
       .then((response) => {
         console.log(response);
-        switch(response.status){
+        switch (response.status) {
           case 201:
             alert("글 등록에 성공했습니다!");
             break;
@@ -84,9 +104,11 @@ const BlogWritePage = () => {
           <TagList TagList={tags} onDelete={setTags} />
         </div>
       </div>
-      <div ref={editorRef}/>
+      <div ref={editorRef} />
       <div className="btn-area grid place-items-end mt-4 mr-2">
-        <button className="border rounded-lg p-2 px-6" onClick={handleSendPost}>전송</button>
+        <button className="border rounded-lg p-2 px-6" onClick={handleSendPost}>
+          전송
+        </button>
       </div>
     </div>
   );
