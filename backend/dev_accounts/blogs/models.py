35071d65cc7a20b8aa710b397fc9d3743b4af2be
정재현 <rubat0@naver.com>
2023-01-31 12:51:53 +0900

    fix: article - tags blank 처리

--- backend/dev_accounts/blogs/models.py
+++ backend/dev_accounts/blogs/models.py
@@ -12,7 +12,7 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     hit = models.IntegerField(default=1)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_articles", blank=True)
-    tags = models.ManyToManyField(Tag, related_name="articles")
+    tags = models.ManyToManyField(Tag, related_name="articles", blank=True)
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
