51a3312d9e8f49be60bf0619aaa569134f83a236
박규영 <magic9282m@gmail.com>
2023-02-02 07:41:18 +0900

    feat: Editor 별도 컴포넌트화에 따른 Editor 값 읽어오기 위해 state 설정함수 prop으로 전달하는 부분 추가

--- frontend/morehae_app/src/pages/Blog/BlogWritePage.jsx
+++ frontend/morehae_app/src/pages/Blog/BlogWritePage.jsx
@@ -7,14 +7,11 @@ import TagCombobox from "@/components/Blog/TagCombobox";
 import TagList from "../../components/Blog/TagList";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import PostEditor from "../../components/Blog/PostEditor";
 
-const MAX_UPLOAD_IMAGE_SIZE = 5242880; // 1024 * 1024 * 5. 5MB로 제한
-const IMAGE_TYPES = ["image/png", "image/jpeg"];
+
 const BlogWritePage = () => {
-  const editorRef = useRef();
   const navigate = useNavigate();
-  const [editor, setEditor] = useState();
-  const [isEditorRendered, setisEditorRendered] = useState(false);
   // 제목 관련 State
   const [title, setTitle] = useState("");
   const [titleTouched, setTitleTouched] = useState("");
@@ -24,61 +21,10 @@ const BlogWritePage = () => {
   const [thumbnail, setThumbnail] = useState("");
   const [thumbnailSrc, setThumbailSrc] = useState("");
   const [tags, setTags] = useState([]);
+  const [editor, setEditor]= useState();                                                                                                                                                                         
   // const request = http(process.env.REACT_APP_BLOG_URL);
   const request = httpWithURL("http://i8d212.p.ssafy.io:9000/blogs");
-  const hookMap = {
-    addImageBlobHook: (blob, callback) => {
-      console.log(blob);
-      console.log(blob.size);
-      console.log(encodeURIComponent(blob.name));
-      if (blob.size > MAX_UPLOAD_IMAGE_SIZE) {
-        alert("첨부하는 이미지는 5MB 이하여야 합니다.");
-        return;
-      }
-      if (!IMAGE_TYPES.includes(blob.type)) {
-        alert("이미지는 png, jpeg, jpg 형식만 첨부할 수 있습니다.");
-        return;
-      }
-      const body = {
-        image: blob,
-      };
-      request
-        .post("article/temp-img", body, {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => response.data)
-        .then((data) => {
-          // callback(`${process.env.REACT_APP_SERVER_URL}${data.image}`, blob.name);
-          callback(`${"http://i8d212.p.ssafy.io:9000"}${data.image}`, blob.name);
-        })
-        .catch((e) => {
-          callback("image-load-fail", "이미지 로딩 실패.");
-        });
-    },
-  };
 
-  // editorRef가 할당되어 있는 element(<div ref={editorRef} />)가 mount 되어 있어야 new Editor()를 통해
-  // 에디터 생성하고 보여주는게 가능하므로 useEffect에서 new Editor를 통한 에디터 객체 생성.
-  useEffect(() => {
-    // 컴포넌트 함수 재 호출(재 렌더링)시 editor 또 생성되는거 방지.
-    if (isEditorRendered) {
-      return;
-    }
-    setEditor(
-      new Editor({
-        el: editorRef.current,
-        previewStyle: "vertical",
-        minHeight: "600px",
-        hideModeSwitch: true,
-        hooks: hookMap,
-        placeholder: "내용을 입력해주세요.",
-      })
-    );
-    setisEditorRendered(true);
-  }, []);
 
   // 제목 관련 Handler 및 Valicdation 값
   const handleTitleInputChange = (e) => {
@@ -151,7 +97,7 @@ const BlogWritePage = () => {
       });
   };
   return (
-    <div className="min-h-screen h-full">
+    <div className="min-h-screen h-full mx-1">
       <div className="text-2xl mb-4 text-center">글 쓰기</div>
       <div className="input-form">
         <div>
@@ -174,23 +120,23 @@ const BlogWritePage = () => {
                 : "제목은 100자 이하여야 합니다."}
             </div>
           )}
-          <div className="grid grid-cols-2 border rounded-lg">
-            <div className="h-60">
+          <div className="grid grid-cols-2 border rounded-lg p-1">
+            <div className="h-60 py-2">
               <span className="text-sm text-gray-500">
                 (선택)썸네일 파일 첨부
                 <br />
               </span>
-              <div className="grid grid-cols-2 h-full">
+              <div className="grid grid-cols-2 h-full border rounded-lg p-1">
                 <input
                   className="file:border-0 file:py-2 file:px-4 file:rounded-lg"
                   type="file"
                   onChange={handleThumbnailChange}
                   accept="image/png, image/jpeg"
                 />
-                <div className="h-full border">
+                <div className="h-full border overflow-hidden">
                   {thumbnail && (
                     <img
-                      className="h-full object-cover"
+                      className="w-full h-full object-contain"
                       src={thumbnailSrc}
                       alt=""
                     />
@@ -198,14 +144,13 @@ const BlogWritePage = () => {
                 </div>
               </div>
             </div>
-            <div className="">
+            <div className="h-60 py-2">
               <span className="text-sm text-gray-500">
                 (선택)글 요약
                 <br />
               </span>
               <textarea
-                className="px-3 py-2 border bg-white text-2xl rounded-md shadow-sm placeholder-slate-40 w-full resize-none"
-                rows="4"
+                className="px-3 py-2 border bg-white text-2xl rounded-md shadow-sm placeholder-slate-40 w-full h-full resize-none"
                 placeholder="글 요약을 입력해주세요."
                 value={overview}
                 onChange={handleOverviewChange}
@@ -220,8 +165,8 @@ const BlogWritePage = () => {
           <TagList TagList={tags} onDelete={setTags} />
         </div>
       </div>
-      <div ref={editorRef} />
-      <div className="btn-area grid place-items-end mt-4 mr-2">
+      <PostEditor onMount={setEditor}/>
+      <div className="btn-area grid place-items-end mt-4">
         <button
           className="border rounded-lg p-2 px-6 bg-blue-500 text-white"
           onClick={handleSendPost}
