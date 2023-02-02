c18fd748756646ab339ce62aa00eb08fea086dda
cloudsoswift <magic9282m@gmail.com>
2023-02-02 11:03:20 +0900

    feat: 블로그 글 작성, 수정 페이지에 컴포넌트 분리한 input들 일부 적용

--- frontend/morehae_app/src/pages/Blog/BlogEditPage.jsx
+++ frontend/morehae_app/src/pages/Blog/BlogEditPage.jsx
@@ -1,99 +1,44 @@
-import Editor from "@toast-ui/editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
-import { useEffect } from "react";
 import { useState } from "react";
-import http from "@/utils/http";
+import httpWithURL from "@/utils/http";
 import TagCombobox from "@/components/Blog/TagCombobox";
-import TagList from "@/components/Blog/TagList";
-import { useRef } from "react";
+import TagList from "../../components/Blog/TagList";
 import { useNavigate } from "react-router-dom";
+import PostEditor from "../../components/Blog/PostEditor";
+import InputThumbnail from "../../components/Blog/InputThumbnail";
+import InputTitle from "../../components/Blog/InputTitle";
+import InputOverview from "../../components/Blog/InputOverview";
 
-const MAX_UPLOAD_IMAGE_SIZE = 5242880; // 1024 * 1024 * 5. 5MB로 제한
-const IMAGE_TYPES = ["image/png", "image/jpeg"]
 const BlogEditPage = () => {
-  const editorRef = useRef();
   const navigate = useNavigate();
-  const [editor, setEditor] = useState();
+  // 제목 관련 State
   const [title, setTitle] = useState("");
-  const [titleTouched, setTitleTouched] = useState("");
+  const [titleIsValid, setTitleIsValid] = useState(false);
+  // 요약글 관련 State
+  const [overview, setOverview] = useState("");
+  const [overviewIsValid, setOverviewIsValid] = useState(false);
+  // Thumbnail 관련 State
+  const [thumbnail, setThumbnail] = useState("");
+
   const [tags, setTags] = useState([]);
-  const [isEditorRendered, setisEditorRendered] = useState(false);
-  const request = http(process.env.REACT_APP_BLOG_URL);
-  console.log(request.defaults);
-  const hookMap = {
-    addImageBlobHook: (blob, callback) => {
-      console.log(blob);
-      console.log(blob.size);
-      console.log(encodeURIComponent(blob.name));
-      if(blob.size > MAX_UPLOAD_IMAGE_SIZE){
-        alert("첨부하는 이미지는 5MB 이하여야 합니다.");
-        return;
-      }
-      if(!IMAGE_TYPES.includes(blob.type)){
-        alert("이미지는 png, jpeg, jpg 형식만 첨부할 수 있습니다.");
-        return;
-      }
-      // if
-      const body = {
-        image: blob,
-      }
-      request.post("article/temp-img", body, {
-        withCredentials: true,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }).then((response)=>response.json())
-      .then((data)=>{callback(data, blob.name)})
-      .catch((e)=>{callback('image-load-fail', '이미지 로딩 실패.')});
-    },
-  };
+  const [editor, setEditor] = useState();
+  // const request = http(process.env.REACT_APP_BLOG_URL);
+  const request = httpWithURL("http://i8d212.p.ssafy.io:9000/blogs");
 
-  // editorRef가 할당되어 있는 element(<div ref={editorRef} />)가 mount 되어 있어야 new Editor()를 통해
-  // 에디터 생성하고 보여주는게 가능하므로 useEffect에서 new Editor를 통한 에디터 객체 생성.
-  useEffect(() => {
-    // 컴포넌트 함수 재 호출(재 렌더링)시 editor 또 생성되는거 방지.
-    if (isEditorRendered){
-      return;
-    }
-    setEditor(
-      new Editor({
-        el: editorRef.current,
-        previewStyle: "vertical",
-        minHeight: "600px",
-        hideModeSwitch: true,
-        hooks: hookMap,
-        placeholder: "내용을 입력해주세요.",
-        // toolbarItems: [
-        //   // 툴바 옵션 설정
-        //   ["heading", "bold", "italic", "strike"],
-        //   ["hr", "quote"],
-        //   ["ul", "ol", "task", "indent", "outdent"],
-        //   ["table", "image", "link"],
-        //   ["code", "codeblock"],
-        // ],
-      })
-    );
-    setisEditorRendered(true);
-  }, []);
-
-  const handleTitleInputChange = (e) => {
-    setTitle(e.target.value);
-  };
-  // 제목 Validation을 위한 값
-  const titleIsEmpty = title.trim() === "";
-  const titleMaxLengthValid = title.trim().length < 100;
-  const titleIsValid = !(titleIsEmpty) && titleMaxLengthValid;
-  const handleTitleInputBlur = (e) => {
-    setTitleTouched(true);
-  }
-  // 
+  //
   const handleSendPost = () => {
-    if(!titleIsValid){
+    if (!titleIsValid) {
       return;
     }
     let body = new FormData();
     body.append("title", title.trim());
     body.append("content", editor.getMarkdown());
+    if (!overviewIsValid) {
+      body.append("overview", "");
+    }
+    if (!thumbnail) {
+      body.append("thumnail", thumbnail);
+    }
     for (let tag of tags) {
       body.append("tags", tag);
     }
@@ -117,38 +62,29 @@ const BlogEditPage = () => {
       });
   };
   return (
-    <div className="min-h-screen h-full">
+    <div className="min-h-screen h-full mx-1">
       <div className="text-2xl mb-4 text-center">글 쓰기</div>
       <div className="input-form">
         <div>
-          <input
-            className="px-3 py-2 bg-white text-2xl rounded-md shadow-sm placeholder-slate-40 w-full"
-            type="text"
-            placeholder="제목을 입력하세요"
-            value={title}
-            onChange={handleTitleInputChange}
-            onBlur={handleTitleInputBlur}
-          />
-          {!titleIsValid && titleTouched && <div className="text-xs text-red-500">{titleIsEmpty ? "제목은 필수입니다." : "제목은 100자 이하여야 합니다."}</div>}
-          <input
-            className="px-3 py-2 bg-white text-2xl rounded-md shadow-sm placeholder-slate-40 w-full"
-            type="text"
-            placeholder="글 요약을 입력해주세요."
-            value={title}
-            onChange={handleTitleInputChange}
-            onBlur={handleTitleInputBlur}
-          />
+          <InputTitle onChange={setTitle} value={title} setValid={setTitleIsValid}/>
+          <div className="grid grid-cols-2 border rounded-lg p-1">
+            <InputThumbnail onChange={setThumbnail} value={thumbnail}/>
+            <InputOverview onChange={setOverview} value={overview} setValid={setOverviewIsValid}/>
+          </div>
         </div>
         <div>
-          <TagCombobox onAddTag={setTags} />
+          <TagCombobox onAddTag={setTags} tagList={tags} />
         </div>
         <div className="h-full my-1">
           <TagList TagList={tags} onDelete={setTags} />
         </div>
       </div>
-      <div ref={editorRef}/>
-      <div className="btn-area grid place-items-end mt-4 mr-2">
-        <button className="border rounded-lg p-2 px-6 bg-blue-500 text-white" onClick={handleSendPost}>
+      <PostEditor onMount={setEditor} />
+      <div className="btn-area grid place-items-end mt-4">
+        <button
+          className="border rounded-lg p-2 px-6 bg-blue-500 text-white"
+          onClick={handleSendPost}
+        >
           등록
         </button>
       </div>
--- frontend/morehae_app/src/pages/Blog/BlogWritePage.jsx
+++ frontend/morehae_app/src/pages/Blog/BlogWritePage.jsx
@@ -1,65 +1,30 @@
-import Editor from "@toast-ui/editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
-import { useEffect } from "react";
 import { useState } from "react";
 import httpWithURL from "@/utils/http";
 import TagCombobox from "@/components/Blog/TagCombobox";
 import TagList from "../../components/Blog/TagList";
-import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import PostEditor from "../../components/Blog/PostEditor";
-
+import InputThumbnail from "../../components/Blog/InputThumbnail";
+import InputTitle from "../../components/Blog/InputTitle";
+import InputOverview from "../../components/Blog/InputOverview";
 
 const BlogWritePage = () => {
   const navigate = useNavigate();
   // 제목 관련 State
   const [title, setTitle] = useState("");
-  const [titleTouched, setTitleTouched] = useState("");
+  const [titleIsValid, setTitleIsValid] = useState(false);
   // 요약글 관련 State
   const [overview, setOverview] = useState("");
+  const [overviewIsValid, setOverviewIsValid] = useState(false);
   // Thumbnail 관련 State
   const [thumbnail, setThumbnail] = useState("");
-  const [thumbnailSrc, setThumbailSrc] = useState("");
+
   const [tags, setTags] = useState([]);
-  const [editor, setEditor]= useState();                                                                                                                                                                         
+  const [editor, setEditor] = useState();
   // const request = http(process.env.REACT_APP_BLOG_URL);
   const request = httpWithURL("http://i8d212.p.ssafy.io:9000/blogs");
 
-
-  // 제목 관련 Handler 및 Valicdation 값
-  const handleTitleInputChange = (e) => {
-    setTitle(e.target.value);
-  };
-  const handleTitleInputBlur = (e) => {
-    setTitleTouched(true);
-  };
-  // - 제목 Validation을 위한 값
-  const titleIsEmpty = title.trim() === "";
-  const titleMaxLengthValid = title.trim().length < 100;
-  const titleIsValid = !titleIsEmpty && titleMaxLengthValid;
-
-  // 요약글(overview) 관련 Handler
-  const handleOverviewChange = (e) => {
-    setOverview(e.target.value);
-  };
-  const overviewIsEmpty = overview.trim() === "";
-  const overviewMaxLengthValid = overview.trim().length;
-
-  // thumbnail 이미지 관련 Handler
-  const handleThumbnailChange = (e) => {
-    const reader = new FileReader();
-    const file = e.target.files[0];
-    if (!file) {
-      setThumbnail(file);
-      setThumbailSrc(file);
-      return;
-    }
-    reader.onloadend = () => {
-      setThumbnail(file);
-      setThumbailSrc(reader.result);
-    };
-    reader.readAsDataURL(file);
-  };
   //
   const handleSendPost = () => {
     if (!titleIsValid) {
@@ -68,7 +33,7 @@ const BlogWritePage = () => {
     let body = new FormData();
     body.append("title", title.trim());
     body.append("content", editor.getMarkdown());
-    if (!overviewIsEmpty) {
+    if (!overviewIsValid) {
       body.append("overview", "");
     }
     if (!thumbnail) {
@@ -101,71 +66,20 @@ const BlogWritePage = () => {
       <div className="text-2xl mb-4 text-center">글 쓰기</div>
       <div className="input-form">
         <div>
-          <span className="text-sm text-gray-500">
-            제목
-            <br />
-          </span>
-          <input
-            className="px-3 py-2 bg-white text-2xl rounded-md shadow-sm placeholder-slate-40 w-full"
-            type="text"
-            placeholder="제목을 입력하세요"
-            value={title}
-            onChange={handleTitleInputChange}
-            onBlur={handleTitleInputBlur}
-          />
-          {!titleIsValid && titleTouched && (
-            <div className="text-xs text-red-500">
-              {titleIsEmpty
-                ? "제목은 필수입니다."
-                : "제목은 100자 이하여야 합니다."}
-            </div>
-          )}
+          <InputTitle onChange={setTitle} value={title} setValid={setTitleIsValid}/>
           <div className="grid grid-cols-2 border rounded-lg p-1">
-            <div className="h-60 py-2">
-              <span className="text-sm text-gray-500">
-                (선택)썸네일 파일 첨부
-                <br />
-              </span>
-              <div className="grid grid-cols-2 h-full border rounded-lg p-1">
-                <input
-                  className="file:border-0 file:py-2 file:px-4 file:rounded-lg"
-                  type="file"
-                  onChange={handleThumbnailChange}
-                  accept="image/png, image/jpeg"
-                />
-                <div className="h-full border overflow-hidden">
-                  {thumbnail && (
-                    <img
-                      className="w-full h-full object-contain"
-                      src={thumbnailSrc}
-                      alt=""
-                    />
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="h-60 py-2">
-              <span className="text-sm text-gray-500">
-                (선택)글 요약
-                <br />
-              </span>
-              <textarea
-                className="px-3 py-2 border bg-white text-2xl rounded-md shadow-sm placeholder-slate-40 w-full h-full resize-none"
-                placeholder="글 요약을 입력해주세요."
-                value={overview}
-                onChange={handleOverviewChange}
-              />
-            </div>
+            <InputThumbnail onChange={setThumbnail} value={thumbnail}/>
+            <InputOverview onChange={setOverview} setValid={setOverviewIsValid} value={overview}/>
           </div>
         </div>
         <div>
-          <TagCombobox onAddTag={setTags} tagList={tags}/>
+          <TagCombobox onAddTag={setTags} tagList={tags} />
         </div>
         <div className="h-full my-1">
           <TagList TagList={tags} onDelete={setTags} />
         </div>
       </div>
-      <PostEditor onMount={setEditor}/>
+      <PostEditor onMount={setEditor} />
       <div className="btn-area grid place-items-end mt-4">
         <button
           className="border rounded-lg p-2 px-6 bg-blue-500 text-white"
