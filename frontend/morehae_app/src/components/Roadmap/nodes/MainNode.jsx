f105714028e094c5f96c972580d47e554fa76257
박규영 <magic9282m@gmail.com>
2023-02-06 00:40:22 +0900

    style: 메인 노드, 서브 노드 스타일링

--- frontend/morehae_app/src/components/Roadmap/nodes/MainNode.jsx
+++ frontend/morehae_app/src/components/Roadmap/nodes/MainNode.jsx
@@ -1,12 +1,13 @@
 import { memo } from "react";
 import { Handle, Position } from "reactflow";
+import galaxyImage from "@/img/planet.png"
 
 const MainNode = ({ data }) => {
   const isEssentialClass = data.isEssential ? "text-red-500" : "text-gray-500"
   return (
-    <div className="px-4 py-2 shadow-md rounded-xl bg-orange-300 border-2" >
+    <div className="px-4 py-2 shadow-md rounded-full border-2 w-96 h-96 text-center bg-cover" style={{backgroundImage:`url(${galaxyImage})`}}>
       <div className={isEssentialClass}>{data.isEssential ? "필수" : "선택"}</div>
-      <div className="flex">
+      <div className="text-center">
         <div className="ml-2">
           <div className="text-lg font-bold">{data.label}</div>
           <div className="text-gray-500">{data.isComplete ? "Clear" : ""}</div>
--- frontend/morehae_app/src/components/Roadmap/nodes/SubNode.jsx
+++ frontend/morehae_app/src/components/Roadmap/nodes/SubNode.jsx
@@ -1,23 +1,37 @@
 import { memo } from "react";
 import { Handle, Position } from "reactflow";
+import essentialImage from "@/img/saturn.png";
+import nonEssentialImage from "@/img/uranus.png";
 
-
-const SubNode = ({data}) => {
-  const isEssentialClass = data.isEssential ? "text-red-500" : "text-gray-500"
-    return (
-      <div className="px-4 py-2 shadow-md rounded-md bg-gray-300 border-2">
-      <div className={isEssentialClass}>{data.isEssential ? "필수" : "선택"}</div>
-      <div className="flex">
-        <div className="ml-2">
-          <div className="text-lg font-bold">{data.label}</div>
-          <div className="text-gray-500">{data.isComplete}</div>
-          <div className="text-gray-500">{data.isEssential}</div>
+const SubNode = ({ data }) => {
+  const isEssentialClass = `justify-center ${data.isEssential ? "text-red-500" : "text-gray-300"}`;
+  return (
+    <div
+      className={
+        "px-2 py-2 shadow-md rounded-full border-2 bg-cover w-20 h-20 text-center grid text-xs"
+      }
+      style={{
+        backgroundImage: `url('${
+          data.isEssential ? essentialImage : nonEssentialImage
+        }')`,
+      }}
+    >
+      <div className={isEssentialClass}>
+        {data.isEssential ? "필수" : "선택"}
+      </div>
+      <div className="place-items-center row-span-2">
+        <div className="">
+          <div className="font-bold">{data.label}</div>
+        </div>
+        <div className="text-gray-300">
+          {data.isComplete ? "이수" : "미이수"}
         </div>
       </div>
 
       <Handle type="target" position={Position.Left} />
+      <Handle type="source" position={Position.Right} />
     </div>
-    )
-}
+  );
+};
 
 export default memo(SubNode);
