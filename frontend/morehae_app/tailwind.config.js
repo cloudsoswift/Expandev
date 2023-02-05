001ec813a0261bd0fdbff62f3586d8be0f5129a2
YongHoon <akatcn@gmail.com>
2023-02-05 13:04:07 +0900

    테마 변경 및 일부 컴포넌트 레이아웃 재조정 & transition 변경으로 인해 좀 더 예뻐짐:)

--- frontend/morehae_app/tailwind.config.js
+++ frontend/morehae_app/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#0d1117',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
