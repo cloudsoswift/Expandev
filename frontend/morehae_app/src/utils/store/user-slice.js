6d95e71a80ff151200fce0b0bfe749d1794b66dc
cloudsoswift <magic9282m@gmail.com>
2023-02-06 14:26:12 +0900

    feat: 로그인시 Refresh Token 쿠키로 설정하도록 추가:

--- frontend/morehae_app/src/utils/store/user-slice.js
+++ frontend/morehae_app/src/utils/store/user-slice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import http, { setAccessToken } from "@/utils/http";
+import Cookies from "universal-cookie";
 
 // CRA에는 dotenv가 내장되어 있어 별도로 호출할 필요가 없다.
 
@@ -20,7 +21,7 @@ export const userLogin = (loginData, navigate) => {
   return (dispatch) => {
     http(process.env.REACT_APP_USER_URL)
       .post("login/", loginData, {
-        // withCredentials: true,
+        withCredentials: true,
       })
       .then(({ status, data }) => {
         switch (status) {
@@ -32,7 +33,14 @@ export const userLogin = (loginData, navigate) => {
       })
       .then((data) => {
         // refresh token을 86,400초 (60초 * 60분 * 24시간 = 1일) 유지되는 httpOnly Cookie로 설정.
-        document.cookie = `refresh_token=${data.refresh_token};path=/;max-age=86400;httpOnly;`;
+        // document.cookie = `refresh_token=${data.refresh_token};path=/;max-age=86400;httpOnly;`;
+        const cookies = new Cookies();
+        cookies.set("refresh_token", data.refresh_token, {
+          path: "/",
+          // httpOnly: true,
+          maxAge: 86400,
+        });
+        console.log(cookies);
         setAccessToken(data.access_token);
         dispatch(
           userActions.setUser({
@@ -62,6 +70,13 @@ export const userLogout = (navigate) => {
             // 로그아웃 후 로드맵 페이지로 이동
             navigate("/roadmap");
         }
+      })
+      .catch((e) => {
+        // Axios Default Header에 설정된 Access Token 삭제
+        setAccessToken();
+        dispatch(userActions.setUser({}));
+        // 로그아웃 후 로드맵 페이지로 이동
+        navigate("/roadmap");
       });
   };
 };
