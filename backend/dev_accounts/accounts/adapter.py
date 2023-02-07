a7dcd19d0e54c7ff65557b407956967b2e7731fb
정재현 <rubat0@naver.com>
2023-02-07 22:25:45 +0900

    fix: User Adapater 수정

--- backend/dev_accounts/accounts/adapter.py
+++ backend/dev_accounts/accounts/adapter.py
@@ -7,7 +7,7 @@ class CustomAccountAdapter(DefaultAccountAdapter):
         data = form.cleaned_data
         user.nickname = data.get('nickname')
         user.login_type = data.get('login_type')
-        user.stat = data.get('stat')
+        # user.stat = data.get('stat')
         # user.phone_number = data.get('phone_number')
         # user.svc_use_pcy_agmt_yn = data.get('svc_use_pcy_agmt_yn')
         # user.ps_info_proc_agmt_yn = data.get('ps_info_proc_agmt_yn')
