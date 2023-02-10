9a096d6ea18fcd949e61b4db87f844bb443c355e
cloudsoswift <magic9282m@gmail.com>
2023-02-10 12:47:58 +0900

    style: 줌 1.5배 이상시 메인 노드 투명화

--- frontend/morehae_app/src/components/Roadmap/nodes/MainNode.jsx
+++ frontend/morehae_app/src/components/Roadmap/nodes/MainNode.jsx
@@ -1,12 +1,14 @@
 import { memo } from "react";
-import { Handle, Position } from "reactflow";
+import { Handle, Position, useStore } from "reactflow";
 import galaxyImage from "@/img/planet.png"
 
+const zoomSelector = (s) => s.transform[2];
 const MainNode = ({ data }) => {
   const isEssentialClass = data.isEssential ? "text-red-500" : "text-gray-500"
-  // const zoomSelector = (s) => s.transform[2];
+  const zoom = useStore(zoomSelector);
+
   return (
-    <div className="px-4 py-2 shadow-md rounded-full border-2 w-96 h-96 text-center bg-cover" style={{backgroundImage:`url(${galaxyImage})`, opacity: 0.6}}>
+    <div className="px-4 py-2 shadow-md rounded-full border-2 w-96 h-96 text-center bg-cover" style={{backgroundImage:`url(${galaxyImage})`, opacity: zoom >= 1.5 ? 0.2 : 1}}>
       <div className={isEssentialClass}>{data.isEssential ? "필수" : "선택"}</div>
       <div className="text-center">
         <div className="ml-2">
