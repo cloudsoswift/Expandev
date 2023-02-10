b0ee78322f431257d17656161704008cdd14416b
cloudsoswift <magic9282m@gmail.com>
2023-02-10 12:44:45 +0900

    style: 노드 사진 변경

--- frontend/morehae_app/src/components/Roadmap/nodes/MainNode.jsx
+++ frontend/morehae_app/src/components/Roadmap/nodes/MainNode.jsx
@@ -4,8 +4,9 @@ import galaxyImage from "@/img/planet.png"
 
 const MainNode = ({ data }) => {
   const isEssentialClass = data.isEssential ? "text-red-500" : "text-gray-500"
+  // const zoomSelector = (s) => s.transform[2];
   return (
-    <div className="px-4 py-2 shadow-md rounded-full border-2 w-96 h-96 text-center bg-cover" style={{backgroundImage:`url(${galaxyImage})`}}>
+    <div className="px-4 py-2 shadow-md rounded-full border-2 w-96 h-96 text-center bg-cover" style={{backgroundImage:`url(${galaxyImage})`, opacity: 0.6}}>
       <div className={isEssentialClass}>{data.isEssential ? "필수" : "선택"}</div>
       <div className="text-center">
         <div className="ml-2">
