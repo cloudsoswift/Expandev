6b9af22cf4badd21d131557ea29b34d8b4b79c7f
cloudsoswift <magic9282m@gmail.com>
2023-02-10 10:13:56 +0900

    test: token refresh용 테스트

--- frontend/morehae_app/src/utils/http.js
+++ frontend/morehae_app/src/utils/http.js
@@ -2,50 +2,65 @@ import axios from "axios";
 import store from "@/utils/store/store";
 import { userActions } from "./store/user-slice";
 
-
 // redux state에 저장해놓은 AccessToken 있으면 반환하는 함수.
 const loadAccessToken = () => {
   const state = store.getState();
   const user = state.user.user;
   const accessToken = state.user.access_token;
-  if(!(user && accessToken)) {
+  if (!(user && accessToken)) {
     // 유저, AccessToken 둘 중 하나만 존재 => 유효하지 않은 로그인 상태
     store.dispatch(userActions.setUser({}));
     store.dispatch(userActions.setAccessToken(""));
   }
   return accessToken;
-}
+};
 
 const httpWithURL = (URL) => {
-  const http =  axios.create({
+  const http = axios.create({
     baseURL: URL,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-  })
-  http.interceptors.request.use((config)=>{
-    // 매 요청 전 AccessToken 있는지 확인하고, 있으면 header에 set하고 없으면 set 안 함.
-    const accessToken = loadAccessToken();
-    config.headers["Authorization"] = accessToken ? `Bearer ${accessToken}` : "";
-    return config;
-  }, (error)=>{
-    return Promise.reject(error);
-  })
+  });
+  http.interceptors.request.use(
+    (config) => {
+      // 매 요청 전 AccessToken 있는지 확인하고, 있으면 header에 set하고 없으면 set 안 함.
+      const accessToken = loadAccessToken();
+      config.headers["Authorization"] = accessToken
+        ? `Bearer ${accessToken}`
+        : "";
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
   http.interceptors.response.use(
     (response) => response,
     (error) => {
       console.log(error);
-      switch(error.response.status){
+      switch (error.response.status) {
         case 401:
-          //Unauthorized
-          // axios.post("http://i8d212.p.ssafy.io:8000/accounts/token/refresh/", )
+        case 403:
+          //Forbbiden
+          axios
+            .post(
+              `${process.env.REACT_APP_USER_URL}verify/token/refresh/cookie`,
+              {},
+              {
+                withCredentials: true,
+              }
+            )
+            .then((response) => {
+              console.log(response);
+            });
           console.log("토큰이 만료되었읍니다.");
           break;
         default:
           break;
       }
       return Promise.reject(error);
-    },
+    }
   );
   return http;
 };
