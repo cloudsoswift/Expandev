d2cf3285702707f790850dc40e1b44e038b373e0
cloudsoswift <magic9282m@gmail.com>
2023-02-14 14:57:30 +0900

    style: 섹션 노드 스타일 임시 설정 및 서브 노드 스타일 임시 설정

--- frontend/morehae_app/src/components/Roadmap/nodes/SectionNode.jsx
+++ frontend/morehae_app/src/components/Roadmap/nodes/SectionNode.jsx
@@ -1,13 +1,13 @@
 import { memo } from "react";
 
 const SectionNode = ({ data }) => {
-  console.log(data);
   return (
     <div
       className={
-        "px-2 py-2 shadow-md border-2 bg-slate-400 w-full h-full text-center opacity-20"
+        "px-2 py-2 shadow-md border-2 w-full h-full text-center text-9xl"
       }
     >
+      {data.label}
     </div>
   );
 };
